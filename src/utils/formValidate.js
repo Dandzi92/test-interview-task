58ea03a1a8efb35753192daba5016e6ee7ec371e
Dzianis Danilkou <d.danilkou@godeltech.com>
2020-06-15 00:04:21 +0300

    fix: correct form validator

--- src/utils/formValidate.js
+++ src/utils/formValidate.js
@@ -1,14 +1,14 @@
 export const formValidate = values => {
   const errors = {};
-  if (!values.destination) {
+  if (!values.destination.trim()) {
     errors.destination = 'Required field!';
   }
 
-  if (!values.link) {
+  if (!values.link.trim()) {
     errors.link = 'Required field!';
   }
 
-  if (!values.textarea) {
+  if (!values.textarea.trim()) {
     errors.textarea = 'Required field!';
   } else if (values.textarea.length > 35) {
     errors.textarea = 'Too verbose description! :)';
